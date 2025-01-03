4a00b7c5b6200e223a5d5a3a869fa39b0aa0d7a3
Rahul Galipelli <rahulgalipelli1603@gmail.com>
2025-01-03 22:44:25 +0530

    Add test to handle custom delimiters and its  implementation

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -6,7 +6,13 @@ public class StringCalculator {
         if(numbers.isEmpty()){
             return 0;
         }
-        String[] nums = numbers.split(",|\n");
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
+        String[] nums = numbers.split(delimiter);
         int sum =0;
         for(String s: nums){
             sum += Integer.parseInt(s);
