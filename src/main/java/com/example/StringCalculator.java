87c466738055e6429ea89b38e938b2c76eaaf3f5
Rahul Galipelli <rahulgalipelli1603@gmail.com>
2025-01-03 22:33:21 +0530

    Add test for single number string and its implementation

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -3,6 +3,9 @@ package com.example;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return 0;
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        return Integer.parseInt(numbers);
     }
 }
